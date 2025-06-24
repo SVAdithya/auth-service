94da0cfdb75c4208f2a1fc983731647eba632e42
Adithya Sv <svadithya93@gmail.com>
2025-06-25 02:22:23 +0530

    Modularize app-service

--- app-service/src/main/java/com/app/auth/AuthServiceApplication.java
+++ app-service/src/main/java/com/app/auth/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.app.auth.auth_service;
+package com.app.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- app-service/src/test/java/com/app/auth/AuthServiceApplicationTests.java
+++ app-service/src/test/java/com/app/auth/AuthServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.app.auth.auth_service;
+package com.app.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+public class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {

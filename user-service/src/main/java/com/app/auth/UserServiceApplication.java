e63419baf0fadf6e7f154b99be598174097c43cc
Adithya Sv <svadithya93@gmail.com>
2025-06-26 02:59:52 +0530

    User module created

---
+++ user-service/src/main/java/com/app/auth/UserServiceApplication.java
@@ -0,0 +1,11 @@
+package com.app.auth;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}

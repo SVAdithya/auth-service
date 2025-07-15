943debe5722490df955270c3e43fd86bc76ec8ee
Adithya Sv <svadithya93@gmail.com>
2025-07-15 23:20:54 +0530

    basic auth structure added

---
+++ app-service/src/main/java/com/app/auth/controller/AuthController.java
@@ -0,0 +1,38 @@
+package com.app.auth.controller;
+
+import com.app.auth.controller.model.LoginRequest;
+import com.app.auth.controller.model.RefreshRequest;
+import com.app.auth.controller.model.TokenResponse;
+import com.app.auth.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+public class AuthController implements AuthApi{
+    private final AuthService authService;
+
+    @Override
+    public ResponseEntity<TokenResponse> authLoginPost(LoginRequest loginRequest) {
+        return ResponseEntity.ok(new TokenResponse(
+                UUID.randomUUID().toString(),
+                UUID.fromString(loginRequest.getUsername()).toString()
+        ));
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> authRefreshPost(RefreshRequest refreshRequest) {
+        return ResponseEntity.ok(new TokenResponse(
+                UUID.randomUUID().toString(),
+                refreshRequest.getRefreshToken()
+        ));
+    }
+
+    @Override
+    public ResponseEntity<Void> authLogoutPost() {
+        return ResponseEntity.noContent().build();
+    }
+}
